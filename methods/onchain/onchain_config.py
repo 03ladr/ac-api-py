047021c5305ef56aca91987b1eef6cb097c63cbf
leo <plasma@void.void>
2022-01-11 21:11:21 -0500

    epoch 3 began

--- methods/onchain/onchain_config.py
+++ methods/onchain/onchain_config.py
@@ -2,11 +2,10 @@
 On-Chain Configuration
 """
 # Config Variables
-from config import WEB3_URL, CONTRACT_ADDRESS
+from os import getenv
 # On-Chain Connectivity/Tooling
 from web3 import Web3
 from .abi import abi
 
-
-w3 = Web3(Web3.HTTPProvider(WEB3_URL))
-contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=abi)
+w3 = Web3(Web3.HTTPProvider(getenv(WEB3_URL))
+contract = w3.eth.contract(address=getenv(CONTRACT_ADDRESS), abi=abi)
